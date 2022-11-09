ab2a397d02b968a32d38f5daffa5e9d73ebb1868
pdemianowicz <pdemianowicz@gmail.com>
2022-11-09 18:52:01 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const navToggle = document.querySelector(".menu-toggle");
+const primaryNav = document.querySelector(".primary-nav");
+
+navToggle.addEventListener("click", () => {
+  const visibility = primaryNav.getAttribute("data-visible");
+
+  if (visibility === "false") {
+    primaryNav.setAttribute("data-visible", true);
+    navToggle.setAttribute("aria-expanded", true);
+  } else if (visibility === "true") {
+    primaryNav.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
+  }
+});
